1f8db0aa1af1c860561da126577576c3215e380f
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2022-11-27 15:56:08 +0000

    ignore

--- App/Views/arquivo/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arquivo = $viewVar['arquivo'];
-$arquivo = str_replace("-","/",base64_decode($arquivo));
-?>
-
-<div>
-	<iframe style="width: 100vw;height: 100vh" src="http://<?php echo APP_HOST . $arquivo ?>" ></iframe>
-</div>
